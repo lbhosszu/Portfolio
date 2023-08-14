e7e8820ebdaebb1e2a54e3c2a1ea333ba9aaf524
Laszlo Hosszu <laszlohosszu@Laszlos-MBP.station>
2023-08-14 15:36:26 +0100

    minor refactor

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,23 +14,18 @@ const About = () => {
         </div>
 
         <p className="text-xl mt-20">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius,
-          deserunt illum mollitia officiis qui exercitationem perferendis neque
-          quasi a recusandae necessitatibus tempora iusto! Blanditiis error
-          iste, totam fugiat recusandae rerum laborum perferendis molestiae
-          aperiam asperiores nemo. Magni dolor maxime debitis vitae, eaque hic
-          ab mollitia voluptatibus, a nostrum eveniet laborum!
+          A driven individual who is motivated to code and extremely good at problem-solving 
+          as demonstrated through personal and college projects. As a result, competent and able 
+          to work well under pressure. Also, an intellectually creative person with experience 
+          using Adobe programs that enables me to create unique and visually appealing solutions.
         </p>
 
         <br />
 
         <p className="text-xl">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum
-          pariatur, vel similique sint, nobis aspernatur ut praesentium
-          explicabo ipsam aliquid quasi laboriosam et culpa possimus repudiandae
-          quisquam ullam maiores ab unde. Fugiat odio mollitia nemo alias.
-          Commodi facilis atque nulla vero voluptatem explicabo. Quibusdam,
-          magni quo! Eum cupiditate debitis labore.
+          Third year Computer Science student, seeking a six-month internship as a Software Engineer/
+           Web Developer for the year 2024. I am looking for a challenging environment to use my skills 
+           and gain significant training in programming in the professional workplace.
         </p>
       </div>
     </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,12 +12,12 @@ const Home = () => {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm a Full Stack Developer
+            I'm a Full Time Student
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
-            I have 8 years of experience building and desgining software.
-            Currently, I love to work on web application using technologies like
-            React, Tailwind, Next JS and GraphQL.
+            Third year Computer Science student, seeking a six-month internship as a Software Engineer/
+            Web Developer for the year 2024. I am looking for a challenging environment to use my skills 
+           and gain significant training in programming in the professional workplace.
           </p>
 
           <div>
